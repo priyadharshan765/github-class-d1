3a29e038fd4a51b9da0ac3cf8133de1f383bd2f2
Priyadharshan <priyadharshan3204@gmail.com>
2025-08-01 15:01:37 +0530

    i am developer

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("hi to class");
         System.out.println("am i good?");
+        System.out.println("i am developer");
     }
 }
